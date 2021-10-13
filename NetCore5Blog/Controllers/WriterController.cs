65d0852b137101198166dcf4f2cfe650447234e4
Metee19 <77540484+Metee19@users.noreply.github.com>
2021-10-13 14:06:43 +0300

    13 Ekim Güncelleme

---
+++ NetCore5Blog/Controllers/WriterController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NetCore5Blog.Controllers
+{
+
+    public class WriterController : Controller
+    {
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult WritterProfile()
+        {
+            return View();
+        }
+        public IActionResult WriterMail()
+        {
+            return View();
+        }
+        [AllowAnonymous]
+        public IActionResult Test()
+        {
+            return View();
+        }
+    }
+}
